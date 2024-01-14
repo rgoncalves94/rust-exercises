7488624e6c19b07f086583264bfdcdd64757f5a6
Rafael Gonçalves <rafael.goncalves.dev@outlook.com>
2024-01-14 00:49:31 -0300

    refactor: refactoring magic numbers

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -5,21 +5,24 @@ pub struct Clock {
   minutes: i32,
 }
 
+const SIXTY_MINUTES: i32 = 60;
+const TWENTY_FOUR_HOURS: i32 = 24;
+
 impl Clock {
   pub fn new(hours: i32, minutes: i32) -> Self {
-   let (clock_hours, clock_minutes) = calculate_clock(hours * 60 + minutes);
+   let (clock_hours, clock_minutes) = calculate_clock(hours * SIXTY_MINUTES + minutes);
    Clock { hours: clock_hours, minutes: clock_minutes }
   }
   
   pub fn add_minutes(&self, minutes: i32) -> Self {
-    let (clock_hours, clock_minutes) = calculate_clock(self.hours * 60 + self.minutes + minutes);
+    let (clock_hours, clock_minutes) = calculate_clock(self.hours * SIXTY_MINUTES + self.minutes + minutes);
     Clock { hours: clock_hours, minutes: clock_minutes }
   }
 }
 
 pub fn calculate_clock(minutes: i32) -> (i32, i32) {
-  let hours = minutes.div_euclid(60).rem_euclid(24);
-  let minutes_mod = minutes.rem_euclid(60);
+  let hours = minutes.div_euclid(SIXTY_MINUTES).rem_euclid(TWENTY_FOUR_HOURS);
+  let minutes_mod = minutes.rem_euclid(SIXTY_MINUTES);
   (hours, minutes_mod)
 }
 
