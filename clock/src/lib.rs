5d015dff3e5a7ae92189abd55970bff7f78f347d
Rafael Gonçalves <rafael.goncalves.dev@outlook.com>
2024-01-13 23:50:10 -0300

    feat: refactor clock impl to store only minutes and implements manually PartialEq

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,53 +1,51 @@
 use std::fmt;
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug)]
 pub struct Clock {
-  hours: i32,
   minutes: i32,
 }
 
 impl Clock {
   pub fn new(hours: i32, minutes: i32) -> Self {
 
-  let (new_hours, new_minutes) = calculate_clock(hours, minutes);
-
    return  Clock {
-      hours: new_hours,
-      minutes: new_minutes,
+      minutes:  hours * 60 + minutes,
     };
   }
   
   pub fn add_minutes(&self, minutes: i32) -> Self {
-    let (zhours, zminutes)  = calculate_clock(self.hours, self.minutes + minutes);
-  
     Clock { 
-      hours: zhours,
-      minutes: zminutes,
+      minutes: self.minutes + minutes,
     }
   }
 }
 
-pub fn calculate_clock(z_hours: i32, z_minutes: i32) -> (i32, i32) {
-  let total_minutes = z_minutes;
-  let hours_to_add = total_minutes / 60;
-  let mut minutes_mod = total_minutes % 60;
-  let mut hours = z_hours + hours_to_add;
-
-  while hours >= 24 {
-    hours -= 24; 
-  }
+pub fn calculate_clock(minutes: i32) -> (i32, i32) {
+  let mut hours =  minutes / 60 ;
+  let mut minutes_mod = minutes % 60;
 
   while hours <= -24 {
     hours += 24;
   }
 
+  while hours >= 24 {
+    hours -= 24;
+  }
+
   if minutes_mod < 0 {
-    println!("pasosu aqui");
-    hours -=1;
     minutes_mod = 60 + minutes_mod;
+    hours -= 1;
   }
 
   if hours < 0 {
     hours = 24 + hours
+  }
+
+  if hours < 0 {
+    hours *= -1
+  }
+
+  if minutes_mod < 0 {
+    minutes_mod *= -1
   }
 
   (hours, minutes_mod)
@@ -55,9 +53,15 @@ pub fn calculate_clock(z_hours: i32, z_minutes: i32) -> (i32, i32) {
 
 impl fmt::Display for Clock {
   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-    let hours = if self.hours < 10 { format!("0{}", self.hours) } else { self.hours.to_string() };
-    let minutes = if self.minutes < 10 { format!("0{}", self.minutes) } else { self.minutes.to_string() };
-    write!(f, "{}:{}", hours, minutes)
+    let (hours, minutes) = calculate_clock(self.minutes);
+    let str_hours = if hours < 10 { format!("0{}", hours) } else { hours.to_string() };
+    let str_minutes = if minutes < 10 { format!("0{}", minutes) } else { minutes.to_string() };
+    write!(f, "{}:{}", str_hours, str_minutes)
   }
 }
 
+impl PartialEq for Clock {
+  fn eq(&self, other: &Self) -> bool {
+      self.to_string() == other.to_string()
+  }
+}
